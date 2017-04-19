07416e7736df1ce4fb20c161e991cae378803c31
thomas-vl <thomas.v.latum@gmail.com>
2017-04-19 15:26:12 +0200

    .

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ RATE = 16000
 
 p = pyaudio.PyAudio()
 
+def callback(in_data, frame_count, time_info, flag):
+    sample = client.sample(stream=in_data,
+                           encoding=speech.Encoding.LINEAR16,
+                           sample_rate_hertz=16000)
+    results = sample.streaming_recognize(language_code='en-US')
+    for result in results:
+        for alternative in result.alternatives:
+            print('=' * 20)
+            print('transcript: ' + alternative.transcript)
+            print('confidence: ' + str(alternative.confidence))
+    return (audio_data, pyaudio.paContinue)
+
+
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 output=True,
-                frames_per_buffer=CHUNK)
+                stream_callback=callback)
 
 stream.start_stream()
 
@@ -25,17 +38,5 @@ while stream.is_active():
     time.sleep(0.1)
 
 stream.stop_stream()
-
-sample = client.sample(stream=stream,
-                       encoding=speech.Encoding.LINEAR16,
-                       sample_rate_hertz=16000)
-results = sample.streaming_recognize(language_code='en-US')
-for result in results:
-    for alternative in result.alternatives:
-        print('=' * 20)
-        print('transcript: ' + alternative.transcript)
-        print('confidence: ' + str(alternative.confidence))
-
-
 stream.close()
 p.terminate()
