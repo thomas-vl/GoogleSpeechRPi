0c8d2a9db9b5770ae2ad399d4af4cc373b2590ec
thomas-vl <thomas.v.latum@gmail.com>
2017-04-19 16:02:45 +0200

    .

--- main.py
+++ main.py
@@ -21,7 +21,16 @@ wf.setframerate(RATE)
 
 def callback(data, frame_count, time_info, flag):
     wf.writeframes(data)
-
+    with open(wf, 'rb') as stream:
+        sample = client.sample(stream=stream,
+                               encoding=speech.Encoding.LINEAR16,
+                               sample_rate_hertz=16000)
+        results = sample.streaming_recognize(language_code='en-US')
+        for result in results:
+            for alternative in result.alternatives:
+                print('=' * 20)
+                print('transcript: ' + alternative.transcript)
+                print('confidence: ' + str(alternative.confidence))
     return (data, pyaudio.paContinue)
 
 
