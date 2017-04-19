6e12ec8dface2dd91ea54c0419426080ad2a77f9
thomas-vl <thomas.v.latum@gmail.com>
2017-04-19 15:21:46 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from google.cloud import speech
 import pyaudio
+import wave
+import sys
+
 
 client = speech.Client()
 CHUNK = 512
@@ -13,20 +16,26 @@ stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
+                output=True,
                 frames_per_buffer=CHUNK)
 
-print("starting")
-while (True):    
-    sample = client.sample(stream=stream,encoding=speech.Encoding.LINEAR16,sample_rate_hertz=16000)
-    results = sample.streaming_recognize(language_code='en-US',single_utterance=True,)
-    for result in results:
-        for alternative in result.alternatives:
-            print('=' * 20)
-            print('transcript: ' + alternative.transcript)
-            print('confidence: ' + str(alternative.confidence))
+stream.start_stream()
 
-print("closing stream")
+while stream.is_active():
+    time.sleep(0.1)
+
 stream.stop_stream()
-stream.close()
 
+sample = client.sample(stream=stream,
+                       encoding=speech.Encoding.LINEAR16,
+                       sample_rate_hertz=16000)
+results = sample.streaming_recognize(language_code='en-US')
+for result in results:
+    for alternative in result.alternatives:
+        print('=' * 20)
+        print('transcript: ' + alternative.transcript)
+        print('confidence: ' + str(alternative.confidence))
+
+
+stream.close()
 p.terminate()
