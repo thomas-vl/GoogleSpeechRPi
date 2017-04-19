aaf0e84264a6a234a0b7a06c896fe39d8a93f1fc
thomas-vl <thomas.v.latum@gmail.com>
2017-04-19 12:54:54 +0200

    .

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ RATE = 16000
 
 p = pyaudio.PyAudio()
 
-#pStream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,output=True)
+stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,output=True)
 
-#pStream.start_stream()
+stream.start_stream()
 
 print("starting")
 
-with p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,output=True) as stream:
-    stream.start_stream()
-    sample = client.sample(stream=stream,encoding=speech.Encoding.LINEAR16,sample_rate_hertz=16000)
-    results = sample.streaming_recognize(language_code='en-US',single_utterance=True,)
-    for result in results:
-        for alternative in result.alternatives:
-            print('=' * 20)
-            print('transcript: ' + alternative.transcript)
-            print('confidence: ' + str(alternative.confidence))
+stream.start_stream()
+sample = client.sample(stream=stream,encoding=speech.Encoding.LINEAR16,sample_rate_hertz=16000)
+results = sample.streaming_recognize(language_code='en-US',single_utterance=True,)
+for result in results:
+    for alternative in result.alternatives:
+        print('=' * 20)
+        print('transcript: ' + alternative.transcript)
+        print('confidence: ' + str(alternative.confidence))
 
 print("closing stream")
 pStream.stop_stream()
